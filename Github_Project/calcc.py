7849a69f8f3382708e13ef209fc559ecba76a049
vanishacrasta <vanisha.crasta@mavs.uta.edu>
2016-04-15 16:23:52 -0500

    First from my end

--- Github_Project/calcc.py
+++ Github_Project/calcc.py
@@ -1,5 +1,5 @@
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
-
+#Vanisha
 # define functions
 def add(x, y):
    """This function adds two numbers"""
