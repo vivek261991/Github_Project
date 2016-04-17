ad1c9646b6a37bc1fa8a24a1bf333d08a1223f76
vanishacrasta <vanisha.crasta@mavs.uta.edu>
2016-04-17 18:02:06 -0500

    First commit with subtraction function...

---
+++ Github_Project/calccsub.py
@@ -0,0 +1,30 @@
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
+#Vanisha
+# define functions
+def add(x, y):
+   """This function adds two numbers"""
+
+   return x + y
+
+def sub(x, y):
+   """This function subtracts two numbers"""
+
+   return x - y
+
+
+# take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Sub")
+
+choice = str(raw_input("Enter choice(1/2):"));
+
+num1 = int(raw_input("Enter first number: "))
+num2 = int(raw_input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+if choice == '2':
+   print(num1,"-",num2,"=", sub(num1,num2))
+else:
+   print("Invalid input")
