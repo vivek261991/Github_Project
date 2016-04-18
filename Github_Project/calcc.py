f33959879c686ba8126f6b6c0489c9f1c5ceedf5
Pooja Venkatesh <Pooja Venkatesh>
2016-04-17 19:04:43 -0500

    Sub function added to calcc.py

--- Github_Project/calcc.py
+++ Github_Project/calcc.py
@@ -6,17 +6,26 @@ def add(x, y):
 
    return x + y
 
+def sub(x, y):
+   """This function adds two numbers"""
+
+   return x - y
+
 
 # take input from the user
 print("Select operation.")
 print("1.Add")
 
-choice = str(raw_input("Enter choice(1/2/3/4):"));
+print("2.Sub")
+
+choice = str(raw_input("Enter choice(1/2):"));
 
 num1 = int(raw_input("Enter first number: "))
 num2 = int(raw_input("Enter second number: "))
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
+if choice == '2':
+   print(num1,"-",num2,"=", sub(num1,num2))
 else:
    print("Invalid input")
